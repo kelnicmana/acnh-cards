765b484e1afda0ea205f32fab1076169af5699a5
Kellie Manasco <kellie.manasco@gmail.com>
2021-07-07 09:18:27 -0400

    Adds names and card borders. More styling needed.

--- src/Villager.js
+++ src/Villager.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Villager = ({ icon_uri }) => {
+const Villager = ({ icon_uri, name }) => {
   return (
     <div className="singleCard">
+      <h2>{name["name-USen"]}</h2>
       <img src={icon_uri} alt="#" />
     </div>
   );
